84e221619024dd39626dcd6e34185908ba85b889
Matt Hamlin <hamlim@outlook.com>
2019-01-31 20:41:53 -0500

    Remove old packages setup

--- packages/simple-site/src/file-context.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-
-export const fileContext = createContext({
-  files: [],
-})
--- packages/simple-site/src/header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const { createElement: h } = React
-
-export default function Header() {
-  return h('header', null, h('a', { href: '/' }, h('mark', null, "Matt Hamlin's Blog")))
-}
