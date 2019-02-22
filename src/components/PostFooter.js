e1a8ebbde5319d9132b0e5842b62a7f32e81fe3b
Matt Hamlin <hamlim@outlook.com>
2019-02-21 19:26:39 -0500

    Fix ssr access to the location of the page

--- src/components/PostFooter.js
+++ src/components/PostFooter.js
@@ -1,24 +1,31 @@
 import React from 'react'
+import { Location } from '@reach/router'
 
 export default function Footer() {
-  const twitterLink = `https://mobile.twitter.com/search?q=${encodeURIComponent(
-    window.location.href
-  )}`
   return (
-    <>
-      <hr />
-      <p>
-        Follow the discussion on this blog post on{' '}
-        <a href={twitterLink}>twitter</a>.
-      </p>
-      <p>
-        Have feedback about this post or questions, don't hesitate to reach out
-        on{' '}
-        <a href="https://mobile.twitter.com/immatthamlin">
-          Twitter (@immatthamlin)
-        </a>
-        !
-      </p>
-    </>
+    <Location>
+      {({ location }) => {
+        const twitterLink = `https://mobile.twitter.com/search?q=${encodeURIComponent(
+          location.href
+        )}`
+        return (
+          <>
+            <hr />
+            <p>
+              Follow the discussion on this blog post on{' '}
+              <a href={twitterLink}>twitter</a>.
+            </p>
+            <p>
+              Have feedback about this post or questions, don't hesitate to
+              reach out on{' '}
+              <a href="https://mobile.twitter.com/immatthamlin">
+                Twitter (@immatthamlin)
+              </a>
+              !
+            </p>
+          </>
+        )
+      }}
+    </Location>
   )
 }
