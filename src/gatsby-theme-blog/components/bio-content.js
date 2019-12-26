9473284aa767cb0028d6f516ff8a5ee4327045ed
Matt Hamlin <matthewjameshamlin@gmail.com>
2019-12-26 09:59:34 -0800

    Update biocontent

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -5,7 +5,7 @@ export default () => (
   <Fragment>
     <Styled.h1>Hey 👋🏼</Styled.h1>
     Hey there, I'm Matt. I'm currently living and working in Boston. I am a
-    Senior Software Engineer working on the Frontend Design Systems team at
+    Staff Software Engineer working on the Frontend Design Systems team at
     Wayfair. In my free time I like to work on several different{' '}
     <Styled.a href="https://github.com/hamlim">side projects</Styled.a>, and
     somehow find time to write some blog posts as well.
